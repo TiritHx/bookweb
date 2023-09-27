61b3463c9fa0125a592a960ea1d3d108fa9b79df
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-09-27 14:30:43 +0200

    update
    fixed tile
    cleaned up images folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useState, setState } from 'react';
+import { useEffect, useState } from 'react';
 import Menu from './components/Menu';
 import Tile from './components/Tile';
 
@@ -8,16 +8,6 @@ function App() {
   const [_title, changeTitle] = useState('qwe');
   const [_src, changeSrc] = useState('tre');
   const [_rating, changeRating] = useState(0); // podączyć te 3 zmienne
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3001/posts")
-  //   .then(response => console.log(response))
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .then(response => updateDATA(response))
-  //   .then(x => console.log(DATA))
-  //  .catch(error => console.log(error))
-  // }, []);
 
   const getData = () => {
     var requestOptions = {
@@ -28,7 +18,6 @@ function App() {
     fetch("http://localhost:3001/posts", requestOptions)
       .then((response) => response.json())
       .then((result) => updateDATA(result))
-      .then(console.log(DATA))
       .catch((error) => console.log("error", error));
   };
 
@@ -47,7 +36,6 @@ function App() {
     console.log('wcisłem');
     fetch("http://localhost:3001/posts", requestOptions)
     .then(updateDATA([...DATA, {title: _title, src: _src, rating: _rating}]))
-    .then(console.log(DATA))
     .catch(error => console.log("error", error));    
   };
   
@@ -55,7 +43,7 @@ function App() {
     <div>
       <Menu />
       <div className='container'> 
-        {DATA.map((x) => <Tile title={x.title} src={x.src} rating={x.rating} key={x.id} />)}
+        {DATA.map((x, index) => <Tile title={x.title} src={x.src == undefined ? '' : x.src} rating={x.rating} key={index} />)}
         <button onClick={SendData}></button>
       </div>
       
