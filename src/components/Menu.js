7326aa5d4099733a5e944ef49e9e81533883a492
Dominik <90091417+TiritHx@users.noreply.github.com>
2023-10-04 10:09:40 +0200

    BIG UPDATE
    git merge finally completed
    added functioning menu
    optimised
    added some css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Menu() {
   return (
-    <div>
-
+    <div id="menu">
+      <Link to={"/"} >Główna</Link>
+      <Link to={"/add"} >Dodaj</Link>
     </div>
   );
 }
