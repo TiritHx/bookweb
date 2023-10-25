3e26a570d4fa644651422a83c7cd552f24d8e61b
Jakub Palka <97618894+PalkaNivien@users.noreply.github.com>
2023-10-25 13:07:23 +0200

    rating fix (test)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <div>
-
+    <div id="menu">
+      <Link to={'/'}>Główna</Link>
+      <Link to={'/add'}>Dodaj</Link>
     </div>
   );
 }
